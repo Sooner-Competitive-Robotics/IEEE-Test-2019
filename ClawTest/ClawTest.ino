435de907dacf4aedff5dbac10baffa2f9e1e90e3
Kichlids <32001401+Kichlids@users.noreply.github.com>
2018-10-06 13:39:14 -0500

    Added test for claw

---
+++ ClawTest/ClawTest.ino
@@ -0,0 +1,29 @@
+#define pinNum 1
+
+Servo clawServo;
+
+void setup()
+{
+	clawServo.attach(pinNum);
+}
+
+void loop()
+{
+	// Grabbing
+	for (int angle = 0; angle <= 180; angle++)
+	{
+		clawServo.write(angle);
+		
+		delay(15);
+	}
+	
+	//Releasing
+	for (int angle = 180; angle >= 0; angle--)
+	{
+		clawServo.write(angle);
+		
+		delay(15);
+	}
+	
+	while (true) {};
+}
