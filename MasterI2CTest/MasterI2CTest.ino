28ebfefac8de7f24bfebee40ac1d1c9a32d14a68
josephchang98 <32658356+josephchang98@users.noreply.github.com>
2018-10-08 15:15:17 -0500

    Simple test on master arduino end to test I2C

---
+++ MasterI2CTest/MasterI2CTest.ino
@@ -0,0 +1,27 @@
+#include <Wire.h>
+//this is arbitrary; slaveAddress is dictated by user in slave's code
+byte slaveAddress = 8;
+
+//to send integer types of greater than 1 byte, need to use bitwise operations to send and receive
+//Website for help on sending data:
+//https://thewanderingengineer.com/2015/05/06/sending-16-bit-and-32-bit-numbers-with-arduino-i2c/
+uint8_t rpm = 25;
+int8_t distance = 12;
+
+void setup() {
+  // put your setup code here, to run once:
+  Wire.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Wire.beginTransmission(slaveAddress);
+  //send RPM
+  Wire.write(rpm);
+  delay(100);
+  //send distance
+  Wire.write(distance);
+  Wire.endTransmission();
+
+  while(true){};
+}
