ae8a9489392e198bda98b05c9d1ae33e33278a23
Hambonaire <32001455+Hambonaire@users.noreply.github.com>
2018-10-09 22:42:47 -0500

    Pin and Serial fixes

--- FowardsAndBackwards/FowardsAndBackwards.ino
+++ FowardsAndBackwards/FowardsAndBackwards.ino
@@ -1,15 +1,18 @@
 //#include <RobotLib.h>
 #include "StepperMotorDrivetrain.h"
+bool turnDone = false;
 
 StepperMotorDrivetrain drivetrain;
 
+
 void setup()
 {
+  Serial.begin(9600);
   //Initialize motors and basic drivetrain functions
-  drivetrain.initBackRight(5, 4, 3, 2);
-  drivetrain.initBackLeft(43, 42, 41, 40);
-  drivetrain.initFrontRight(11, 10, 9, 8);
-  drivetrain.initFrontLeft(50, 51, 52, 53);
+  drivetrain.initFrontLeft(2, 3, 4, 5);
+  drivetrain.initFrontRight(40, 41, 42, 43);
+  drivetrain.initBackLeft(8, 9, 10, 11);
+  drivetrain.initBackRight(50, 51, 52, 53);
   drivetrain.setRPM(25);
 }
 
--- StrafeTest/StrafeTest.ino
+++ StrafeTest/StrafeTest.ino
@@ -1,80 +1,65 @@
 //#include <RobotLib.h>
 #include "StepperMotorDrivetrain.h"
 
+bool turnDone = false;
+
 StepperMotorDrivetrain drivetrain;
+
 
 void setup()
 {
   Serial.begin(9600);
   //Initialize motors and basic drivetrain functions
-  drivetrain.initBackRight(5, 4, 3, 2);
-  drivetrain.initBackLeft(43, 42, 41, 40);
-  drivetrain.initFrontRight(11, 10, 9, 8);
-  drivetrain.initFrontLeft(50, 51, 52, 53);
+  drivetrain.initFrontLeft(2, 3, 4, 5);
+  drivetrain.initFrontRight(40, 41, 42, 43);
+  drivetrain.initBackLeft(8, 9, 10, 11);
+  drivetrain.initBackRight(50, 51, 52, 53);
   drivetrain.setRPM(25);
 }
 
 void loop()
 {
-  Serial.print("Code start");
-  
   // Straight Left Strafe
   drivetrain.setRPM(25);
-  drivetrain.strafe(0, -1, drivetrain.convertInchesToSteps(12));
+  drivetrain.strafe(0, -1, drivetrain.convertInchesToSteps(36));
   delay(500);
 
-  Serial.print("Left Strafe finished");
-  
-   // Straight Right Strafe
+  // Straight Right Strafe
   drivetrain.setRPM(25);
-  drivetrain.strafe(0, 1, drivetrain.convertInchesToSteps(12));
+  drivetrain.strafe(0, 1, drivetrain.convertInchesToSteps(36));
   delay(500);
 
-  Serial.print("Right Strafe finished");
-  
-   // Straight Forwards Strafe
+  // Straight Forwards Strafe
   drivetrain.setRPM(25);
-  drivetrain.strafe(1, 0, drivetrain.convertInchesToSteps(12));
+  drivetrain.strafe(1, 0, drivetrain.convertInchesToSteps(36));
   delay(500);
 
-  Serial.print("Forwards finished");
-   
-   // Straight Backwards Strafe
+  // Straight Backwards Strafe
   drivetrain.setRPM(25);
-  drivetrain.strafe(-1, 0, drivetrain.convertInchesToSteps(12));
+  drivetrain.strafe(-1, 0, drivetrain.convertInchesToSteps(36));
   delay(500);
 
-  Serial.print("Backwards finished");
-  
-   // Straight Forwards Left Strafe
+  // Straight Forwards Left Strafe
   drivetrain.setRPM(25);
-  drivetrain.strafe(1, -1, drivetrain.convertInchesToSteps(12));
+  drivetrain.strafe(1, -1, drivetrain.convertInchesToSteps(36));
   delay(500);
 
-  Serial.print("Diagnoal forward left finished");
-  
-   // Straight Forwards Right Strafe
+  // Straight Forwards Right Strafe
   drivetrain.setRPM(25);
-  drivetrain.strafe(1, 1, drivetrain.convertInchesToSteps(12));
+  drivetrain.strafe(1, 1, drivetrain.convertInchesToSteps(36));
   delay(500);
 
-  Serial.print("Diagonal forward right finished");
-  
-   // Straight Backwards Left Strafe
+  // Straight Backwards Left Strafe
   drivetrain.setRPM(25);
-  drivetrain.strafe(-1, -1, drivetrain.convertInchesToSteps(12));
+  drivetrain.strafe(-1, -1, drivetrain.convertInchesToSteps(36));
   delay(500);
 
-  Serial.print("Diagonal backwards left");
-  
-   // Straight Backwards Right Strafe
+  // Straight Backwards Right Strafe
   drivetrain.setRPM(25);
-  drivetrain.strafe(-1, 1, drivetrain.convertInchesToSteps(12));
+  drivetrain.strafe(-1, 1, drivetrain.convertInchesToSteps(36));
   delay(500);
 
-  Serial.print("Diagonal backwards right");
-
-  while(true){};
+  //while(true){};
 
 }
 
