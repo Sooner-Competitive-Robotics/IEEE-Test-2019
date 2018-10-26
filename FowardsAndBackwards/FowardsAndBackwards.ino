ef1401c98dd5c6a94ef08a9e52987b6844c691ab
josephchang98 <32658356+josephchang98@users.noreply.github.com>
2018-10-25 21:27:54 -0500

    changed pin numbers. Note: Need to make a globals file for pin numbers

--- FowardsAndBackwards/FowardsAndBackwards.ino
+++ FowardsAndBackwards/FowardsAndBackwards.ino
@@ -10,18 +10,18 @@ void setup()
   Serial.begin(9600);
   //Initialize motors and basic drivetrain functions
   drivetrain.initFrontLeft(2, 3, 4, 5);
-  drivetrain.initFrontRight(40, 41, 42, 43);
+  drivetrain.initFrontRight(33, 32, 31, 30);
   drivetrain.initBackLeft(8, 9, 10, 11);
-  drivetrain.initBackRight(50, 51, 52, 53);
+  drivetrain.initBackRight(53, 52, 51, 50);
   drivetrain.setRPM(25);
 }
 
 void loop()
 {
-  drivetrain.step(drivetrain.convertInchesToSteps(120), drivetrain.convertInchesToSteps(120));
+  drivetrain.step(drivetrain.convertInchesToSteps(12), drivetrain.convertInchesToSteps(12));
   delay(500);
 
-  drivetrain.step(drivetrain.convertInchesToSteps(-120), drivetrain.convertInchesToSteps(-120));
+  drivetrain.step(drivetrain.convertInchesToSteps(-12), drivetrain.convertInchesToSteps(-12));
   delay(500);
 
   while(true){};
--- StrafeTest/StrafeTest.ino
+++ StrafeTest/StrafeTest.ino
@@ -11,19 +11,20 @@ void setup()
   Serial.begin(9600);
   //Initialize motors and basic drivetrain functions
   drivetrain.initFrontLeft(2, 3, 4, 5);
-  drivetrain.initFrontRight(40, 41, 42, 43);
+  drivetrain.initFrontRight(33, 32, 31, 30);
   drivetrain.initBackLeft(8, 9, 10, 11);
-  drivetrain.initBackRight(50, 51, 52, 53);
+  drivetrain.initBackRight(53, 52, 51, 50);
   drivetrain.setRPM(25);
 }
 
 void loop()
 {
+
   // Straight Left Strafe
   drivetrain.setRPM(25);
   drivetrain.strafe(0, -1, drivetrain.convertInchesToSteps(36));
   delay(500);
-
+  
   // Straight Right Strafe
   drivetrain.setRPM(25);
   drivetrain.strafe(0, 1, drivetrain.convertInchesToSteps(36));
@@ -59,7 +60,7 @@ void loop()
   drivetrain.strafe(-1, 1, drivetrain.convertInchesToSteps(36));
   delay(500);
 
-  //while(true){};
+  while(true){};
 
 }
 
