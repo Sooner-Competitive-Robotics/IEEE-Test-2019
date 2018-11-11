69eec50ae6e2d69d82ca9bdfc5922f37c0fa45dd
Kevin Cronch <Kevin.c.cronch@gmail.com>
2018-11-11 04:22:23 -0600

    Update MasterI2CTest/MasterI2CTest.ino

--- MasterI2CTest/MasterI2CTest.ino
+++ MasterI2CTest/MasterI2CTest.ino
@@ -5,13 +5,15 @@ byte slaveAddress = 8;
 //to send integer types of greater than 1 byte, need to use bitwise operations to send and receive
 //Website for help on sending data:
 //https://thewanderingengineer.com/2015/05/06/sending-16-bit-and-32-bit-numbers-with-arduino-i2c
-//uint8_t ledState = 1;
-uint8_t rpm = 25;
+uint8_t ledState = 2;
+//uint8_t rpm = 25;
 //uint8_t distance = 12;
+boolean successfulTransmission = false;
 
 void setup() {
   // put your setup code here, to run once:
   Wire.begin();
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -19,12 +21,14 @@ void loop() {
   // put your main code here, to run repeatedly:
   Wire.beginTransmission(slaveAddress);
   //send RPM
-  //Wire.write(ledState);
-  Wire.write(rpm);
-  //delay(100);
+  delay(10);
+  Wire.write(ledState);
+  //Wire.write(rpm);
+  delay(10);
   //send distance
   //Wire.write(distance);
-  Wire.endTransmission();
-  delay(100);
+  successfulTransmission = Wire.endTransmission();
+  delay(80);
+  Serial.println(successfulTransmission);
   //while(true){};
 }
