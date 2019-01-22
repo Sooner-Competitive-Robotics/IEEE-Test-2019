75ac1b2e401763ab357538a6805608c1c3d85721
Hambonaire <32001455+Hambonaire@users.noreply.github.com>
2019-01-22 17:49:13 -0600

    Revert to 4 motors on one Nano

--- StrafeTest/StrafeTest.ino
+++ StrafeTest/StrafeTest.ino
@@ -6,54 +6,83 @@ bool turnDone = false;
 
 void setup()
 {
-  Serial.begin(9600);
-  robotSetup();
+	Wire.begin(001);
+	Wire.onRecieve(testEvent);
+	
+	robotSetup();
+	
+	//Serial.begin(9600);
+	//robotSetup();
 }
 
 void loop()
 {
 
-  // Straight Left Strafe
-  drivetrain.setRPM(25);
-  drivetrain.strafe(0, -1, drivetrain.convertInchesToSteps(36));
-  delay(500);
-  
-  // Straight Right Strafe
-  drivetrain.setRPM(25);
-  drivetrain.strafe(0, 1, drivetrain.convertInchesToSteps(36));
-  delay(500);
+	/*
+	// Straight Left Strafe
+	drivetrain.setRPM(25);
+	drivetrain.strafe(0, -1, drivetrain.convertInchesToSteps(36));
+	delay(500);
 
-  // Straight Forwards Strafe
-  drivetrain.setRPM(25);
-  drivetrain.strafe(1, 0, drivetrain.convertInchesToSteps(36));
-  delay(500);
+	// Straight Right Strafe
+	drivetrain.setRPM(25);
+	drivetrain.strafe(0, 1, drivetrain.convertInchesToSteps(36));
+	delay(500);
 
-  // Straight Backwards Strafe
-  drivetrain.setRPM(25);
-  drivetrain.strafe(-1, 0, drivetrain.convertInchesToSteps(36));
-  delay(500);
+	// Straight Forwards Strafe
+	drivetrain.setRPM(25);
+	drivetrain.strafe(1, 0, drivetrain.convertInchesToSteps(36));
+	delay(500);
 
-  // Straight Forwards Left Strafe
-  drivetrain.setRPM(25);
-  drivetrain.strafe(1, -1, drivetrain.convertInchesToSteps(36));
-  delay(500);
+	// Straight Backwards Strafe
+	drivetrain.setRPM(25);
+	drivetrain.strafe(-1, 0, drivetrain.convertInchesToSteps(36));
+	delay(500);
 
-  // Straight Forwards Right Strafe
-  drivetrain.setRPM(25);
-  drivetrain.strafe(1, 1, drivetrain.convertInchesToSteps(36));
-  delay(500);
+	// Straight Forwards Left Strafe
+	drivetrain.setRPM(25);
+	drivetrain.strafe(1, -1, drivetrain.convertInchesToSteps(36));
+	delay(500);
 
-  // Straight Backwards Left Strafe
-  drivetrain.setRPM(25);
-  drivetrain.strafe(-1, -1, drivetrain.convertInchesToSteps(36));
-  delay(500);
+	// Straight Forwards Right Strafe
+	drivetrain.setRPM(25);
+	drivetrain.strafe(1, 1, drivetrain.convertInchesToSteps(36));
+	delay(500);
 
-  // Straight Backwards Right Strafe
-  drivetrain.setRPM(25);
-  drivetrain.strafe(-1, 1, drivetrain.convertInchesToSteps(36));
-  delay(500);
+	// Straight Backwards Left Strafe
+	drivetrain.setRPM(25);
+	drivetrain.strafe(-1, -1, drivetrain.convertInchesToSteps(36));
+	delay(500);
 
-  while(true){};
+	// Straight Backwards Right Strafe
+	drivetrain.setRPM(25);
+	drivetrain.strafe(-1, 1, drivetrain.convertInchesToSteps(36));
+	delay(500);
 
+	while(true){};
+	*/
 }
 
+void testEvent()
+{
+	int counter = 0;
+	
+	int byteA = 0;
+	int byteB = 0;
+	
+    while(Wire.available()) {
+		int number = Wire.read();
+
+        if (counter == 0){
+			byteA = number;
+        }
+
+        if (counter == 1){
+			byteB = number;
+        }
+
+        counter++;
+    }
+	
+	drivetrain.strafe(byteB, byteC, drivetrain.convertInchesToSteps(byteA));
+}
