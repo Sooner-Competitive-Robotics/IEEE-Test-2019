9ba51e428e6fcff0c2457b5eec0b97f075aa52ce
Hambonaire <32001455+Hambonaire@users.noreply.github.com>
2019-03-06 20:09:25 -0600

    Update StrafeTest.ino

--- StrafeTest/StrafeTest.ino
+++ StrafeTest/StrafeTest.ino
@@ -9,7 +9,7 @@ void setup()
 	//Wire.begin(001);
 	//Wire.onRecieve(testEvent);
 	
-	//robotSetup();
+	robotSetup();
 	
 	//Serial.begin(9600);
 	//robotSetup();
@@ -18,12 +18,23 @@ void setup()
 void loop()
 {
   //gyroInterrupt();
-	
-	// Straight Left Strafe
-	//drivetrain.setRPM(25);
-	//drivetrain.strafe(1,0, drivetrain.convertInchesToSteps(36));
-	//delay(500);
+
 /*
+  while (!turnDone)
+  {
+    updateGyro();
+    turnDone = drivetrain.stepToAngle(90, GYRO_YAW);
+  }
+  Serial.println("Done");
+  turnDone = false;
+  resetGyro();
+  delay(100);
+ */ 
+	// Straight Left Strafe
+	drivetrain.setRPM(25);
+	drivetrain.strafe(1,0, drivetrain.convertInchesToSteps(36));
+	delay(500);
+
 	// Straight Right Strafe
 	drivetrain.setRPM(25);
 	drivetrain.strafe(0, 1, drivetrain.convertInchesToSteps(36));
@@ -60,30 +71,5 @@ void loop()
 	delay(500);
 
 	while(true){};
-	*/
+
 }
-/*
-void testEvent()
-{
-	int counter = 0;
-	
-	int byteA = 0;
-	int byteB = 0;
-	
-    while(Wire.available()) {
-		int number = Wire.read();
-
-        if (counter == 0){
-			byteA = number;
-        }
-
-        if (counter == 1){
-			byteB = number;
-        }
-
-        counter++;
-    }
-	
-	drivetrain.strafe(byteB, byteC, drivetrain.convertInchesToSteps(byteA));
-}
-*/
