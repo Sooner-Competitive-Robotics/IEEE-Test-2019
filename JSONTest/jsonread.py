7f3ed43206649f295dcbab1871bf197cdd3cc06a
josephchang98 <32658356+josephchang98@users.noreply.github.com>
2019-03-20 19:25:15 -0500

    made a new pinTest to test pins and changed robotsetup to drivesetup in strafe test

--- JSONTest/jsonread.py
+++ JSONTest/jsonread.py
@@ -1,13 +1,42 @@
 import json
+import math
 from pprint import pprint
 
-with open('jsontest.json') as data_file:
-	data = json.load(data_file)
-pprint(data)
+def jsonread():
+	print("hi\n")
+	with open('jsontest.json') as data_file:
+		data = json.load(data_file)
+	pprint(data)
+	
+	print("\n")
+	print(data["size"])
+	print("\n")
+	print(data["x coords"])
+	print("\n")
+	print(data["y coords"])
+	print("\n")
+	
+	return data
+	
+# Get JSON file location
+data = jsonread()
+size = data["size"]
+minDist = 96 # inches
+minX = 4
+minY = 4
 
+print(data["x coords"][5])
+
+while (size > 0):
+	print(size)
+	print("\n")
+	dist = abs(math.sqrt(math.pow((data["x coords"][size - 1] - 4), 2)+math.pow((data["x coords"][size - 1] - 4), 2)))
+	if (dist < minDist):
+		minX = data["x coords"][size - 1]
+		minY = data["y coords"][size - 1]
+	size = size - 1
+
+print(minX)
 print("\n")
-print(data["size"])
+print(minY)
 print("\n")
-print(data["x coords"])
-print("\n")
-print(data["y coords"])
---
+++ pinTest/pinTest.ino
@@ -0,0 +1,71 @@
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);  
+  pinMode(A4, OUTPUT);
+  pinMode(A6, OUTPUT);
+  pinMode(A7, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(2, HIGH);
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
+  digitalWrite(13, HIGH);
+  digitalWrite(A0, HIGH);
+  digitalWrite(A1, HIGH);
+  digitalWrite(A2, HIGH);
+  digitalWrite(A3, HIGH);  
+  digitalWrite(A4, HIGH);
+  digitalWrite(A6, HIGH);
+  digitalWrite(A7, HIGH);
+  
+  delay(20);
+
+  digitalWrite(2, LOW);
+  digitalWrite(3, LOW);
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(7, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
+  digitalWrite(13, LOW);
+  digitalWrite(A0, LOW);
+  digitalWrite(A1, LOW);
+  digitalWrite(A2, LOW);
+  digitalWrite(A3, LOW);  
+  digitalWrite(A4, LOW);
+  digitalWrite(A6, LOW);
+  digitalWrite(A7, LOW);
+
+  delay(20);
+  
+}
