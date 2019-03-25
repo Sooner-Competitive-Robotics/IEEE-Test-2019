034311c064838b3a9d782565434538263470816d
Kichlids <32001401+Kichlids@users.noreply.github.com>
2019-03-25 18:50:57 -0500

    Deleted delay

--- SlaveMotorTest_02/SlaveMotorTest_02.ino
+++ SlaveMotorTest_02/SlaveMotorTest_02.ino
@@ -10,64 +10,64 @@ int dataA, dataB, dataC, dataD = 0;
 
 void setup()
 {
-	state = Idle;
-	driveSetup();
-	Wire.begin(10);
-	Wire.setClock(100000);
-	Wire.onReceive(testEvent);
+  state = Idle;
+  driveSetup();
+  Wire.begin(10);
+  Wire.setClock(100000);
+  Wire.onReceive(testEvent);
 }
 
 void loop()
 {
-	if (state == Idle)
-	{
-		// Don't do anything
-	}
-	else if (state == Move)
-	{
-		drivetrain.setRPM(15);
-		drivetrain.strafe(dataA, dataB, drivetrain.convertInchesToSteps(dataC));
-		delay(drivetrain.calculateStepWait(drivetrain.convertInchesToSteps(dataC)));
-		state = Idle;
-	}
-	
+  if (state == Idle)
+  {
+    // Don't do anything
+  }
+  else if (state == Move)
+  {
+    drivetrain.setRPM(15);
+    drivetrain.strafe(dataA, dataB, drivetrain.convertInchesToSteps(dataC));
+    state = Idle;
+  }
+  delay(10);
 }
 
 void testEvent()
 {
-	int counter = 0;
-	
-	int byteA = 0; //strafe forward
-	int byteB = 0; //strafe sideways
-    int byteC = 0; // distance
-	int byteD = 0; // angle
+  int counter = 0;
   
-	while(Wire.available()) 
-	{
-		int number = (int)Wire.read();
+  int byteA = 0; //strafe forward
+  int byteB = 0; //strafe sideways
+    int byteC = 0; // distance
+  int byteD = 0; // angle
+  
+  while(Wire.available()) 
+  {
+    int number = (int)Wire.read();
+    Serial.println(number);
 
-		if (counter == 1){
-			byteA = number;
-		}
-	
-		if (counter == 2){
-			byteB = number;
-		}
-	
-		if (counter == 3){
-			byteC = number;
-		}
-	
-		if (counter == 4){
-			byteD = number;
-			break;
-		}
-		counter++;
-	}
-	dataA = byteA;
-	dataB = byteB;
-	dataC = byteC;
-	dataD = byteD;
-	
-	state = Move;
+    if (counter == 1){
+      byteA = number;
+    }
+  
+    if (counter == 2){
+      byteB = number;
+    }
+  
+    if (counter == 3){
+      byteC = number;
+    }
+  
+    if (counter == 4){
+      byteD = number;
+      break;
+    }
+    counter++;
+  }
+  dataA = byteA;
+  dataB = byteB;
+  dataC = byteC;
+  dataD = byteD;
+  
+  state = Move;
 }
