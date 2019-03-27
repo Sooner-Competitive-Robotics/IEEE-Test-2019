359467f009e7ead621af9c966a8e844c7ac65924
josephchang98 <Jospeh.H.Chang-1@ou.edu>
2019-03-27 17:42:53 -0500

    fixed wait

--- MasterI2CTest3.py
+++ MasterI2CTest3.py
@@ -2,6 +2,7 @@ import smbus
 import time
 import startup
 from vision import vision
+from NanoManager import NanoManager
 from gpiozero import Button
 from picamera import PiCamera
 import RPi.GPIO as GPIO
@@ -16,12 +17,16 @@ camera.resolution = (600, 600)
 camera.rotation = 180
 
 myvision = vision()
+myNano = NanoManager()
 
-print("First data incoming")
-bus.write_i2c_block_data(address1, 0, [0 ,1, 12])
-print("First data sent. sleeping for 10s")
-time.sleep(10)
-print("Second data incoming")
-bus.write_i2c_block_data(address1, 0, [1, 0, 12])
-print("Second data sent. sleeping for 4s")
-time.sleep(4)
+
+def driveRobot(address, forward, strafe, dist):
+    bus.write_i2c_block_data(address, 0, [forward, strafe, dist])
+    waitTime = myNano.getWaitTime(myNano.convertInchesToSteps(dist))
+    print("waiting: " + str(waitTime))
+    time.sleep(waitTime)
+
+driveRobot(address1, 1, 0, 12)
+driveRobot(address1, 0, 1, 12)
+driveRobot(address1, -1, 0, 12)
+driveRobot(address1, 0, -1, 12)
--- NanoManager.py
+++ NanoManager.py
@@ -2,12 +2,9 @@ class NanoManager:
 	
 	# Calculate the amount of time the amount of steps will take
 	def getWaitTime(self, steps):
-		rpm = 15.0
-		STEPS_PER_REVOLUTION = 200
-		# Determine how many seconds we want to wait and convert to integer. 
-		totalTime = float(steps / STEPS_PER_REVOLUTION) / rpm * 60
-		T = (totalTime / steps) / 2
-		return int(T) + 1
+                rpm = 15.0
+                STEPS_PER_REVOLUTION = 200
+                return int(steps/200*(15/rpm)*3) + 2
 	
 	# Calculate the number of steps needed to go x inches
 	def convertInchesToSteps(self, inches):
